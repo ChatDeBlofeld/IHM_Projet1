2480846ecc37b93f506e8eb83fbf9c21819c44bc
Gildas <gildas.houlmann@gmail.com>
2020-11-08 21:37:38 +0100

    merged stuff

--- deadlinehandler.h
+++ deadlinehandler.h
@@ -13,9 +13,7 @@ private:
 public:
     explicit DeadlineHandler(QObject* parent = 0) : QObject(parent){}
 
-    Q_PROPERTY(int years READ years WRITE setYears NOTIFY yearsChanged)
-    Q_PROPERTY(int days READ days WRITE setDays NOTIFY daysChanged)
-    Q_PROPERTY(int hours READ hours WRITE setHours NOTIFY hoursChanged)
+
 
 };
 
